7eb723dbd4815ffe16306931573eaf7a713a9ab8
thanadol <thanadol.saen@kmutt.ac.th>
2022-09-13 20:09:09 +0700

    Final

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,7 +1,12 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
-  // ojiojioj
+  if (word === null || undefined){
+    return undefined
+}else if(startWord === word.slice(0,startWord.length)){
+    return word;
+}else if (word){
+    return startWord + word;
+}
 }
 module.exports = fillStartWord
